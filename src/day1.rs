6cfc4a928fd0e382bf75595763aa1e3c4180d140
Linus Unnebäck <linus@folkdatorn.se>
2021-12-07 17:34:56 +0100

    🎉 Add solution to Day 1 - Part 2

--- src/day1.rs
+++ src/day1.rs
@@ -12,3 +12,14 @@ pub fn part1(input: &str) -> Result<u64, ParseIntError> {
         .map(|slice| if slice[1] > slice[0] { 1 } else { 0 })
         .sum())
 }
+
+#[aoc(day1, part2)]
+pub fn part2(input: &str) -> Result<u64, ParseIntError> {
+    Ok(input
+        .split_whitespace()
+        .map(|depth| depth.parse::<u64>())
+        .collect::<Result<Vec<_>, _>>()?
+        .windows(4)
+        .map(|slice| if (slice[1] + slice[2] + slice[3]) > (slice[0] + slice[1] + slice[2]) { 1 } else { 0 })
+        .sum())
+}
