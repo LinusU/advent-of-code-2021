00f84d35e068c32a08bc82a73ade79f66c307a11
Linus Unnebäck <linus@folkdatorn.se>
2021-12-07 17:28:13 +0100

    🎉 Add solution to Day 1 - Part 1

---
+++ src/day1.rs
@@ -0,0 +1,14 @@
+use std::num::ParseIntError;
+
+use aoc_runner_derive::aoc;
+
+#[aoc(day1, part1)]
+pub fn part1(input: &str) -> Result<u64, ParseIntError> {
+    Ok(input
+        .split_whitespace()
+        .map(|depth| depth.parse::<u64>())
+        .collect::<Result<Vec<_>, _>>()?
+        .windows(2)
+        .map(|slice| if slice[1] > slice[0] { 1 } else { 0 })
+        .sum())
+}
