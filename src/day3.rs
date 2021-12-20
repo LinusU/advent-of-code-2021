02f3ff2bc1708de27b9a01d94b671c096c4e87cb
Linus Unnebäck <linus@folkdatorn.se>
2021-12-20 21:40:07 +0100

    🐛 Avoid premature collect

--- src/day3.rs
+++ src/day3.rs
@@ -4,32 +4,28 @@ use aoc_runner_derive::aoc;
 
 #[aoc(day3, part1)]
 pub fn part1(input: &str) -> Result<u64, ParseIntError> {
-    let numbers = input
-        .split_whitespace()
-        .map(|s| u64::from_str_radix(s, 2))
-        .collect::<Result<Vec<_>, _>>()?;
+    let mut num_lines = 0;
+    let mut counts = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-    let counts =
-        numbers
-            .iter()
-            .fold((0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), |mem, number| {
-                return (
-                    mem.0 + ((number & 0x800) >> 11),
-                    mem.1 + ((number & 0x400) >> 10),
-                    mem.2 + ((number & 0x200) >> 9),
-                    mem.3 + ((number & 0x100) >> 8),
-                    mem.4 + ((number & 0x080) >> 7),
-                    mem.5 + ((number & 0x040) >> 6),
-                    mem.6 + ((number & 0x020) >> 5),
-                    mem.7 + ((number & 0x010) >> 4),
-                    mem.8 + ((number & 0x008) >> 3),
-                    mem.9 + ((number & 0x004) >> 2),
-                    mem.10 + ((number & 0x02) >> 1),
-                    mem.11 + (number & 0x01),
-                );
-            });
+    for number in input.split_whitespace() {
+        let number = u64::from_str_radix(number, 2)?;
 
-    let half = numbers.len() as u64 / 2;
+        num_lines += 1;
+        counts.0 += (number & 0x800) >> 11;
+        counts.1 += (number & 0x400) >> 10;
+        counts.2 += (number & 0x200) >> 9;
+        counts.3 += (number & 0x100) >> 8;
+        counts.4 += (number & 0x080) >> 7;
+        counts.5 += (number & 0x040) >> 6;
+        counts.6 += (number & 0x020) >> 5;
+        counts.7 += (number & 0x010) >> 4;
+        counts.8 += (number & 0x008) >> 3;
+        counts.9 += (number & 0x004) >> 2;
+        counts.10 += (number & 0x02) >> 1;
+        counts.11 += number & 0x01;
+    }
+
+    let half = num_lines as u64 / 2;
 
     let gamma = (if counts.0 > half { 2048 } else { 0 }
         + if counts.1 > half { 1024 } else { 0 }
