0fe231bbecb9b7f955c8c99f2072419ddad419c0
Linus Unnebäck <linus@folkdatorn.se>
2021-12-24 00:35:41 +0100

    🎉 Add solution to Day 6 - Part 1

---
+++ src/day6.rs
@@ -0,0 +1,36 @@
+use std::num::ParseIntError;
+
+use aoc_runner_derive::aoc;
+
+#[aoc(day6, part1)]
+pub fn part1(input: &str) -> Result<u64, ParseIntError> {
+    let mut fishes = [0; 9];
+
+    for age in input.split(',') {
+        fishes[age.parse::<usize>()?] += 1;
+    }
+
+    for _ in 0..80 {
+        fishes = [
+            fishes[1],
+            fishes[2],
+            fishes[3],
+            fishes[4],
+            fishes[5],
+            fishes[6],
+            fishes[7] + fishes[0],
+            fishes[8],
+            fishes[0],
+        ]
+    }
+
+    Ok(fishes.iter().sum())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(super::part1("3,4,3,1,2"), Ok(5934));
+    }
+}
